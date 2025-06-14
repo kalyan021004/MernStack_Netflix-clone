b811ad08a0dc07caf6fa915320a9811fe7405e86
kalyan021004 <kalyan021004@gmail.com>
2025-06-14 22:38:36 +0530

    Add feature

--- backend/app.js
+++ backend/app.js
@@ -65,7 +65,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
