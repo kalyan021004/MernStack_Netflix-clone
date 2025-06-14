6d599512c916690416c86719cea56ec994d63013
kalyan021004 <kalyan021004@gmail.com>
2025-06-14 22:49:50 +0530

    Add feature

--- backend/app.js
+++ backend/app.js
@@ -71,14 +71,13 @@ if (process.env.NODE_ENV === "production") {
   
   // Try different variations of the catch-all route
   // Option 1: Simple asterisk
-  app.get("*", (req, res) => {
+  /*pp.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  });*/
   
-  // If Option 1 doesn't work, try Option 2:
-  // app.get("/:path*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+   app.get("/:path*", (req, res) => {
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
 }
 
 const PORT = process.env.PORT || 8080;
